cdd3dd2b5ced90858d1c168ac539a58cdf76ab99
Marcel Cornett <marcornett@gmail.com>
2020-07-09 16:12:10 -0400

    Revert "created navbar"
    This reverts commit eb4215756369307b32fd50e9146ecce408fc6a90.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navigation from "./components/navigation/Navigation"
-
 
 // TODO: ADD REDUX STORE
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      
+      <h1>Capstone</h1>
       {/* Potentially use Navigation */}
     </div>
   );
