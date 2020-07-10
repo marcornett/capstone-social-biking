ebf96f8110f0de2eabcb8335f2b699eadcc1652b
Marcel Cornett <marcornett@gmail.com>
2020-07-10 16:24:54 -0400

    Made fields required.

--- front_end/src/components/sign-in/SignIn.js
+++ front_end/src/components/sign-in/SignIn.js
@@ -24,15 +24,15 @@ export const SignIn = () => {
                     <h2 id="logo">Logo</h2>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control required type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
-                  </Form.Text>
+                    </Form.Text>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control required type="password" placeholder="Password" />
                     </Form.Group>
                     <Form.Group controlId="formBasicCheckbox">
                         {/* <Form.Check type="checkbox" label="Save Password" /> */}
