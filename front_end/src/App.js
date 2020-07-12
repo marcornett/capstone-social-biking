01a06eeda7b1fd8b003b6e98b3f013bfe1af0b73
gordomathurin <mathuringordon@gmail.com>
2020-07-12 11:05:47 -0400

    loader created

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navigation } from "./components/navigation"
 
+
 // TODO: ADD REDUX STORE
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+
     </div>
   );
 }
