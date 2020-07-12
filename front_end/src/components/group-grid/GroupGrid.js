3a5eeccd90d86e7e33a26f20565b6de1b79d2cc7
Marcel Cornett <marcornett@gmail.com>
2020-07-12 16:12:07 -0400

    Replaced a tags with react router dom link tags to access params.

--- front_end/src/components/group-grid/GroupGrid.js
+++ front_end/src/components/group-grid/GroupGrid.js
@@ -1,43 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './GroupGrid.css'
 
 export const GroupGrid = () => {
+
     return (
         <div id="groupContent">
             <h5>Groups Near You</h5>
             <section id="grid">
                 <div className="column">
-                    <a href="http://">
+                    <Link to="/group/:testGroup1">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
-                    <a href="http://">
+                    </Link>
+                    <Link to="/group/:testGroup2">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
-                    <a href="http://">
+                    </Link>
+                    <Link to="/group/:testGroup3">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
+                    </Link>
                 </div>
                 <div className="column">
-                    <a href="http://">
+                    <Link to="/group/:testGroup4">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
-                    <a href="http://">
+                    </Link>
+                    <Link to="/group/:testGroup5">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
-                    <a href="http://">
+                    </Link>
+                    <Link to="/group/:testGroup6">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
+                    </Link>
                 </div>
                 <div className="column">
-                    <a href="http://">
+                    <Link to="/group/:testGroup7">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
-                    <a href="http://">
+                    </Link>
+                    <Link to="/group/:testGroup8">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
-                    <a href="http://">
+                    </Link>
+                    <Link to="/group/:testGroup9">
                         <img src="https://i.ytimg.com/vi/FpGGIA2j0c4/maxresdefault.jpg" alt="" />
-                    </a>
+                    </Link>
                 </div>
             </section>
         </div>
