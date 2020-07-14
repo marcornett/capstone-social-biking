65d2b1c0a34e81b8b4c037276b49d713e12b926a
gordomathurin <mathuringordon@gmail.com>
2020-07-14 19:08:17 -0400

    removed lines from screen page

--- front_end/src/screens/HomeSignedOut.js
+++ front_end/src/screens/HomeSignedOut.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { GroupGrid } from '../components/group-grid'
 import './Home.css'
-import { SI_HomeBanner } from "../components/si_homeBanner/SI_HomeBanner"
 
 export const HomeSignedOut = () => (
     <div className="home">
