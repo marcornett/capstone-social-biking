abce62492abec37d3b614b7c3f8ce643f45f8b70
Marcel Cornett <marcornett@gmail.com>
2020-07-14 12:55:32 -0400

    Unused imports were removed to stop eslint notifications. Punctuation was adjusted on saves by eslint.

--- front_end/src/components/group-banner/GroupBanner.js
+++ front_end/src/components/group-banner/GroupBanner.js
@@ -1,38 +1,38 @@
 import React from 'react'
-import {Row,Container,Col, Card, Image, Button, Jumbotron } from 'react-bootstrap'
+import { Row, Container, Col, Card, Image, Button } from 'react-bootstrap'
 import './GroupBanner.css'
 
-export const GroupBanner = () =>{
-  return(
+export const GroupBanner = () => {
+  return (
     <div className="groupBanner">
-    
-    <Container fluid>
-    
-  <Row>
-    <Col md={true}>
-    <Card>
-     <Card.Header>Group Name</Card.Header>
-      <Card.Body>
-        <Card.Title>About</Card.Title>
-          <Card.Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur arcu enim, sed accumsan urna gravida at. Nulla semper nisi a justo auctor, at consequat dolor luctus. Aliquam sagittis massa ut tellus rutrum tincidunt ac vel arcu. Pellentesque mattis gravida mauris, id suscipit sapien sodales vitae. Vivamus porta erat sit amet semper aliquam. Vestibulum aliquet mollis ultricies.
+
+      <Container fluid>
+
+        <Row>
+          <Col md={true}>
+            <Card>
+              <Card.Header>Group Name</Card.Header>
+              <Card.Body>
+                <Card.Title>About</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur arcu enim, sed accumsan urna gravida at. Nulla semper nisi a justo auctor, at consequat dolor luctus. Aliquam sagittis massa ut tellus rutrum tincidunt ac vel arcu. Pellentesque mattis gravida mauris, id suscipit sapien sodales vitae. Vivamus porta erat sit amet semper aliquam. Vestibulum aliquet mollis ultricies.
           </Card.Text>
-        <Card.Title>Location</Card.Title>
-          <Card.Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                <Card.Title>Location</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </Card.Text>
-          <Button className="button" variant="primary">Join</Button>
-      </Card.Body>
-      </Card>
-      </Col>
-      <Col>
-    
-      <Image src="https://thumbs.dreamstime.com/b/cyclists-bike-race-banner-style-colorful-illustration-bunch-taking-part-white-background-56476672.jpg" rounded/>
-     
-      </Col>
-  </Row>
-</Container>
-    
-  </div>
+                <Button className="button" variant="primary">Join</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+
+            <Image src="https://thumbs.dreamstime.com/b/cyclists-bike-race-banner-style-colorful-illustration-bunch-taking-part-white-background-56476672.jpg" rounded />
+
+          </Col>
+        </Row>
+      </Container>
+
+    </div>
   )
 }
