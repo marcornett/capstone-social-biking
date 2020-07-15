b9d029b9976eb0725795555cde3717790e53c898
Marcel Cornett <marcornett@gmail.com>
2020-07-15 16:33:28 -0400

    User route with post request to make a user.

---
+++ backend_API/routes/users.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import { UserModel } from '../database/schemas/user'
+
+export default (app) => {
+    const router = express.Router()
+
+    router.post("/", async (req, res) => {
+        const {
+            username,
+            password,
+            email,
+        } = req.body
+        console.log(req.body)
+
+        const user = new UserModel({
+            username,
+            password,
+            email
+        })
+        try {
+            await user.save()
+            res.send(user)
+        } catch (err) {
+            res.status(500).send("Internal Server Error")
+        }
+    })
+
+    router.put("/", async (req, res) => {
+        // Update user image
+        const user = await UserModel.findOneAndUpdate()
+    })
+
+    app.use("/users", router)
+}
