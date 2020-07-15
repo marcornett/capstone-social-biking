3435b83fe7056067c257f0456f46066e765f86d1
gordomathurin <mathuringordon@gmail.com>
2020-07-14 22:05:50 -0400

    added starting code for geolocation api

--- front_end/src/utils/api.js
+++ front_end/src/utils/api.js
@@ -3,3 +3,26 @@ import axios from "axios"
 class API {
 
 }
+
+
+
+
+
+
+
+
+
+
+
+//SETUP API 
+/************************GEOLOCATION SETUP*******************************/
+try {
+    if ('geolocation' in navigator) {
+        console.log("geolocation available")
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position)
+        });
+    }
+} catch (err) {
+    console.log("geolocation not available", err)
+}
