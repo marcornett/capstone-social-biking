0fe448701abd0fb2ecd6afbaaf0e6e772c7e0445
Greg Spurgeon <spurgeon.greg11@gmail.com>
2020-07-16 11:59:28 -0400

    fix GroupPage

--- front_end/src/screens/GroupPage.js
+++ front_end/src/screens/GroupPage.js
@@ -1,13 +1,14 @@
-import React from "react"
-import { EventList } from '../components/event-list'
-import { GroupBanner } from '../components/group-banner'
-
-// Example
-// import {Navbar} from "../components" 
+import React from 'react';
+import { EventList } from '../components/event-list';
+import { GroupBanner } from '../components/group-banner';
+import { NavigationBar } from '../components/navigationBar';
+import { FooterPage } from "../components/footer"
 
 export const GroupPage = () => (
-    <div className="group">
-        <EventList />
-        <GroupBanner/>
-    </div>
-)
+  <div className='group'>
+    <NavigationBar />
+    <GroupBanner />
+    <EventList />
+    <FooterPage />
+  </div>
+);
