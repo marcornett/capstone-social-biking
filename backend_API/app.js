878a4e915c45e9b9326ae47b05faf8056f9af58f
Marcel Cornett <marcornett@gmail.com>
2020-07-17 13:59:13 -0400

    Setting up authtication. Currently have login, get user, and register user working.

--- backend_API/app.js
+++ backend_API/app.js
@@ -2,6 +2,8 @@ import express, { response } from 'express'
 import cors from 'cors'
 import { connectDB } from './database/connect'
 import path from 'path'
+import { UserModel } from './database/schemas/user'
+import { GroupModel } from './database/schemas/group'
 import jwt from 'jsonwebtoken'
 
 // Start App
@@ -9,18 +11,12 @@ const app = express()
 const port = process.env.PORT || 5000
 const CURRENT_SERVER_DIRECTORY = path.resolve(path.dirname("."))
 const STATIC_DIRECTORY = path.resolve(CURRENT_SERVER_DIRECTORY, "backend_API", "public")
+const JWT_SECRET = process.env.JWT_SECRET
 
 // Connect to DB
 connectDB()
 
-// Authentication 
-app.post('/login', (req, res, next) => {
-    const { username, password } = req.body
-    // TODO: work in progress
-    if (users[0].username === username && users[0].password === password) {
-        res.send({ auth: true, token: process.env.NODE_AUTH_TOKEN })
-    }
-})
+const users = [{ username: "John", password: "password1", id: 3 }]
 
 
 // Middleware
@@ -36,7 +32,47 @@ app.use(cors({ origin: true }))
 // group(app)
 // app.use('/groups', group)
 
-app.get('/users', async (req, res) => {
+// User Routes
+
+// Login
+// Working with token
+app.post('/api/auth/login', async (req, res, next) => {
+    const { username, password } = req.body
+
+    await UserModel.findOne({ username }, (err, user) => {
+        // Authentication 
+        if (user.username === username && user.password === password) {
+            const jsonToken = jwt.sign(
+                { payload: { username } },
+                JWT_SECRET,
+                { expiresIn: '1 days' }
+            )
+            res.send({ auth: true, token: jsonToken, username })
+        }
+    })
+})
+
+// Get user
+// Working with token
+app.get('/api/users/:username', async (req, res) => {
+    const token = req.headers["authorization"]
+    if (!token) {
+        return res.send('Not authorized')
+    }
+    try {
+        jwt.verify(token, JWT_SECRET, async function (err, decoded) {
+            await UserModel.findOne({ username: req.params.username }, (err, user) => {
+                res.send(user)
+            })
+        })
+    } catch (err) {
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+// Get All users
+// Working
+app.get('/api/users', async (req, res) => {
     try {
         await UserModel.find((err, users) => {
             if (err) {
@@ -49,47 +85,42 @@ app.get('/users', async (req, res) => {
     }
 })
 
-app.get('/users/:username', async (req, res) => {
-    try {
-        await UserModel.findOne({ username: req.params.username }, (err, user) => {
-            if (err) {
-                res.status(500).send("Internal Server Error")
-            }
-            res.send(user)
-        })
-    } catch (err) {
-        res.status(500).send("Internal Server Error")
-    }
-})
+// Register User
+// Working with token
+app.post("/api/users/register", async (req, res) => {
+    const {
+        username,
+        password,
+        email,
+        image
+    } = req.body
 
-app.post("/users", async (req, res) => {
     try {
-        await UserModel.findOne({ username: req.body.username }, async (err, user) => {
-            if (user) {
-                console.log(user)
-                // TODO: Send plain message if user exists
-                res.format({
-                    'text/plain': function () {
-                        res.send('hey');
-                    }
-                })
-            } else {
-                const {
-                    username,
-                    password,
-                    email,
-                    image
-                } = req.body
-
+        await UserModel.findOne({ username }, async (err, user) => {
+            if (!user) {
+                const jsonToken = jwt.sign(
+                    { payload: { username } },
+                    JWT_SECRET,
+                    { expiresIn: '1 days' }
+                )
                 const newUser = new UserModel({
                     username,
                     password,
                     email,
-                    image
+                    image,
+                    token: jsonToken
                 })
-
                 await newUser.save()
                 res.send(newUser)
+
+            } else {
+                console.log(user)
+                // TODO: Send plain message if user exists
+                res.format({
+                    'text/plain': function () {
+                        res.send('User already exists');
+                    }
+                })
             }
         })
     } catch (err) {
@@ -97,7 +128,8 @@ app.post("/users", async (req, res) => {
     }
 })
 
-app.put("/users/:username/picture", async (req, res) => {
+// Add user picture
+app.put("/api/users/:username/picture", async (req, res) => {
     // Update user image
     try {
         await UserModel.findOneAndUpdate(
@@ -116,7 +148,8 @@ app.put("/users/:username/picture", async (req, res) => {
     }
 })
 
-app.delete('/users/:username', async (req, res) => {
+// Delete user
+app.delete('/api/users/:username', async (req, res) => {
     try {
         await UserModel.findOneAndDelete({ username: req.params.username }, (err, user) => {
             if (err) {
@@ -124,6 +157,61 @@ app.delete('/users/:username', async (req, res) => {
             }
             res.send(user)
         })
+    } catch (err) {
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+
+// Group Routes
+
+app.get("/api/groups", async (req, res) => {
+    try {
+        await GroupModel.find(
+            // use location to get groups in area
+            req.body.location ? req.body : null,
+            // { location: req.body.location },
+            function (err, groups) {
+                res.send(groups)
+            })
+    } catch (err) {
+        res.send('Internal Server Error')
+    }
+})
+
+app.get("/api/groups/:groupName", async (req, res) => {
+    try {
+        await GroupModel.find(
+            req.params ? req.params : null,
+            function (err, groups) {
+                res.send(groups)
+            })
+    } catch (err) {
+        res.send('Internal Server Error')
+    }
+})
+
+// Makes a group but front-end won't use until we include a make group component
+app.post("/api/groups", async (req, res) => {
+    const {
+        groupName,
+        location,
+        image,
+        about,
+        eventList
+    } = req.body
+
+    const group = new GroupModel({
+        groupName,
+        location,
+        image,
+        about,
+        eventList
+    })
+
+    try {
+        await group.save()
+        res.send(group)
     } catch (err) {
         res.status(500).send("Internal Server Error")
     }
