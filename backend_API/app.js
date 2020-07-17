3c05b009f8c5c39096d80c3cff18e62f9dfd135f
Marcel Cornett <marcornett@gmail.com>
2020-07-17 09:07:36 -0400

    Added enviroment variable for database url and working on jwt tokens.

--- backend_API/app.js
+++ backend_API/app.js
@@ -4,6 +4,7 @@ import { connectDB } from './database/connect'
 import user from './routes/users'
 import group from './routes/groups'
 import path from 'path'
+import jwt from 'jsonwebtoken'
 
 // Start App
 const app = express()
@@ -13,6 +14,16 @@ const STATIC_DIRECTORY = path.resolve(CURRENT_SERVER_DIRECTORY, "backend_API", "
 
 // Connect to DB
 connectDB()
+
+// Authentication 
+app.post('/login', (req, res, next) => {
+    const { username, password } = req.body
+    // TODO: work in progress
+    if (users[0].username === username && users[0].password === password) {
+        res.send({ auth: true, token: process.env.NODE_AUTH_TOKEN })
+    }
+})
+
 
 // Middleware
 app.use(express.json())
