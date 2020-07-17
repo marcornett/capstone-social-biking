a8dd0a97d18acfe6889f4dad5f64a40f17be4f8d
gordomathurin <mathuringordon@gmail.com>
2020-07-16 21:39:01 -0400

    used a different method for geolocation

--- front_end/src/components/navigationBar/NavigationBar.js
+++ front_end/src/components/navigationBar/NavigationBar.js
@@ -1,28 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Form, Col, Image, Nav, Navbar } from 'react-bootstrap';
 import cyclelogo3 from "../assets/cyclelogo2.png"
 import { SignIn } from '../sign-in'
 import { Register } from '../register'
 import SetProfileIcon from '../profile-Icon/ProfileIcon'
 import './NavigationBar.css';
-import { useLocation } from "../../utils/index"
+import { LocateUserPosition } from '../../utils/index'
 
 
 export const NavigationBar = () => {
-  /*******************************************************/
-  //geolocation is placed here for will be changed
-  const locationCheck = useLocation()
-
-  useEffect(() => {
-    if (!locationCheck) return
-  }, [locationCheck])
-  /*******************************************************/
-
+  LocateUserPosition()
   return (
     <React.Fragment>
       <Navbar className="nav-bar" expand="lg">
         <Col xs={6} md={4}>
-          <Image src={cyclelogo3} alt="logo"  />
+          <Image src={cyclelogo3} alt="logo" />
         </Col>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -30,14 +22,14 @@ export const NavigationBar = () => {
 
           </Nav>
           <Form inline>
-          <Nav.Link className="nav-links" href="/">Home</Nav.Link>
-          <SignIn />
-          <Register />
-          <SetProfileIcon />
-          
+            <Nav.Link className="nav-links" href="/">Home</Nav.Link>
+            <SignIn />
+            <Register />
+            <SetProfileIcon />
+
           </Form>
-          </Navbar.Collapse>
-          </Navbar>
+        </Navbar.Collapse>
+      </Navbar>
     </React.Fragment>
   );
 }
