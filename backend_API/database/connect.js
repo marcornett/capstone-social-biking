b4748dfcb3a249c095172ab3afd6b3fadb499b22
Marcel Cornett <marcornett@gmail.com>
2020-07-17 08:56:49 -0400

    Changed to environment variable.

--- backend_API/database/connect.js
+++ backend_API/database/connect.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
-const URI = "mongodb+srv://marcornett:r6CqDCV2fYOiE1uM@cluster0.rofev.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const URL = process.env.DB_CONNECTION
 
 export const connectDB = async () => {
     mongoose.connect(
-        URI,
+        URL,
         { useUnifiedTopology: true, useNewUrlParser: true },
         () => {
             console.log(`Connected to DB /`)
