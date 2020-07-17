8c3882c60fb7bcc20fcca19a11222e85ab13e6c1
Marcel Cornett <marcornett@gmail.com>
2020-07-16 20:58:50 -0400

    Updated routes to one file and adding routes as I go. Had issue importing routes to one file with error 'app.use() isn't a function.' The fix for now was to just use one file.

--- backend_API/app.js
+++ backend_API/app.js
@@ -1,9 +1,11 @@
 import express, { response } from 'express'
+import cors from 'cors'
 import { Schema } from 'mongoose'
 import { connectDB } from './database/connect'
-import user from './routes/users'
-import group from './routes/groups'
 import path from 'path'
+import { UserModel } from './database/schemas/user'
+// import user from './routes/users'
+// import group from './routes/groups'
 
 // Start App
 const app = express()
@@ -18,16 +20,62 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(STATIC_DIRECTORY))
+app.use(cors({ origin: true }))
 
 // routes
-user(app)
-app.use('/users', user)
+// user(app)
+// app.use('/users', user)
 
-group(app)
-app.use('/groups', group)
+// group(app)
+// app.use('/groups', group)
 
-app.get('/', (req, res) => {
-    res.send('Capstone Backend')
+app.get('/users', async (req, res) => {
+    try {
+        UserModel.find((err, users) => {
+
+            res.send(users)
+        })
+    } catch (err) {
+        res.send("Internal Server Error")
+    }
+})
+
+app.get('/users/:username', async (req, res) => {
+    console.log(req.params.username)
+    try {
+        UserModel.findOne({ username: req.params.username }, (err, user) => {
+            res.send(user)
+        })
+    } catch (err) {
+        res.send("Internal Server Error")
+    }
+})
+
+app.post("/users", async (req, res) => {
+    const {
+        username,
+        password,
+        email,
+        image
+    } = req.body
+
+    const user = new UserModel({
+        username,
+        password,
+        email,
+        image
+    })
+    try {
+        await user.save()
+        res.send(user)
+    } catch (err) {
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+app.put("/users/:username/picture", async (req, res) => {
+    // Update user image
+    const user = await UserModel.findOneAndUpdate()
 })
 
 // App Listening
