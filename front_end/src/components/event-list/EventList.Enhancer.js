7b0461dfbfe28b81b8f4ee0bcd77bcfcef819d2b
Greg Spurgeon <spurgeon.greg11@gmail.com>
2020-07-17 18:26:49 -0400

    setup the structure for all the enhancers for each component that will need access to store

---
+++ front_end/src/components/event-list/EventList.Enhancer.js
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+
+
+
+const mapStateToProps = (state) => ({
+ 
+});
+
+const mapDispatchToProps = {
+  
+};
+
+export const enhancer = connect(mapStateToProps, mapDispatchToProps);
