4bb814ccd3108df431678f0f22d20463b3cc0c8d
Marcel Cornett <marcornett@gmail.com>
2020-07-16 22:13:00 -0400

    Mongoose needs an option set if you don't want to use the useFindAndModify which replace the findOneAndUpdate / findOneAndDelete. Updated this to false.

--- backend_API/database/connect.js
+++ backend_API/database/connect.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 
 const URI = "mongodb+srv://marcornett:r6CqDCV2fYOiE1uM@cluster0.rofev.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
+mongoose.set('useFindAndModify', false)
+
 export const connectDB = async () => {
     mongoose.connect(
         URI,
