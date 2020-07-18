43aa1db993c05ff0954dbf324df2a4c57ea5016e
Greg Spurgeon <spurgeon.greg11@gmail.com>
2020-07-18 14:49:10 -0400

    fix register enhancer error

--- front_end/src/components/register/Register.Enhancer.js
+++ front_end/src/components/register/Register.Enhancer.js
@@ -1,7 +1,6 @@
 import { connect } from "react-redux";
 import { registerUser } from "../../redux/actions/user"
 
-// add the action and reducer first
 const mapDispatchToProps = {
   registerUser
 };
