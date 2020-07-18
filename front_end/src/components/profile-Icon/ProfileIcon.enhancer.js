16bf582b385182dfaceae4a7669aa57873f00d45
Greg Spurgeon <spurgeon.greg11@gmail.com>
2020-07-18 15:39:10 -0400

    added enhancer and access to store to profileIcon component

--- front_end/src/components/profile-Icon/ProfileIcon.enhancer.js
+++ front_end/src/components/profile-Icon/ProfileIcon.enhancer.js
@@ -1,7 +1,18 @@
 import { connect } from "react-redux";
+import { getUser, uploadUserImage, deleteUser } from "../../redux/actions/user"
 
 const mapStateToProps = (state) => ({
   image:state.user.image,
+  username:state.user.username,
+  password:state.user.username,
+  email:state.user.email,
+  groups:state.user.groups,
 });
 
-export const enhancer = connect(mapStateToProps);
+const mapDispatchToProps={
+  getUser,
+  uploadUserImage,
+  deleteUser
+}
+
+export const enhancer = connect(mapStateToProps, mapDispatchToProps);
