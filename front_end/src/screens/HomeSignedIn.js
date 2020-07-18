a4ea14a0132861519cec445311fba39bd3f8aebd
Marcel Cornett <marcornett@gmail.com>
2020-07-18 10:12:37 -0400

    Made your groups component.

--- front_end/src/screens/HomeSignedIn.js
+++ front_end/src/screens/HomeSignedIn.js
@@ -2,11 +2,13 @@ import React from "react"
 import { GroupGrid } from '../components/group-grid'
 import { NavigationBar } from "../components/navigationBar"
 import { FooterPage } from "../components/footer"
+import { YourGroups } from "../components/your-groups"
 
 export const HomeSignedIn = () => (
     <div className="home">
         <NavigationBar />
+        <YourGroups />
         <GroupGrid />
-        <FooterPage/>
+        <FooterPage />
     </div>
 )
